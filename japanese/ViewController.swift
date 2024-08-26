e85b53e096a2c23657f633794b0dca56a79fd31b
tong <zz472780@gmail.com>
2024-08-26 21:39:26 +0800

    Initial Commit

---
+++ japanese/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  japanese
+//
+//  Created by 陳曉潼 on 2024/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
